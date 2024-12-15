aa6766e0e2788fe4dc4b5092a411c49cad86dd83
Griffo <marceloramalho.info@gmail.com>
2024-12-15 13:00:20 -0300

    Creating a mother class, using inheritance to make code cleaner. Also removing one underscore to avoid name mangling

--- Python/Python_4_Avançando_em_OO/modelo.py
+++ Python/Python_4_Avançando_em_OO/modelo.py
@@ -1,50 +1,39 @@
-class Filme:
+class ProgramaDeTV:
+    def __init__(self, nome, ano):
+        self._nome = nome.title() #just one underscore to show that it is a private attribute but still let sn class use it whithou name englibng
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes += 1
+
+
+class Filme(ProgramaDeTV):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
+        self._nome = nome.title() #_ProgramaDeTV__nome if name engling is used like __name
         self.ano = ano
         self.duracao = duracao
-        self.__likes = 0
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
+        self._likes = 0
 
 
-class Serie:
+class Serie(ProgramaDeTV):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.__likes = 0
-
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
+        self._likes = 0
 
 
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
