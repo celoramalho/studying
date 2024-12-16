c75989630c47ac6b3171247141195a3d7fb82d91
anubis_greyignis <marceloramalho.info@gmail.com>
2024-12-16 17:35:14 -0300

    Creating playlist class

--- Python/Python_4_Avançando_em_OO/modelo.py
+++ Python/Python_4_Avançando_em_OO/modelo.py
@@ -47,16 +47,30 @@ class Serie(ProgramaDeTV):
     def __str__(self):
         return f"{self._nome} - {self.ano} - {self.temporadas} seasons - {self._likes} Likes" 
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.programas = programas
+
+    def tamanho(self):
+        return len(self.programas)
 
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
-vingadores.dar_like()
-
-
 atlanta = Serie("atlanta", 2018, 2)
+tmep = Filme("Todo mundo em pânico", 1999, 100)
+demolidor = Serie("demolidor", 2016, 2)
+
+
+vingadores.dar_like()
+vingadores.dar_like()
+vingadores.dar_like()
+demolidor.dar_like()
+tmep.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist("fim de semana", filmes_e_series)
 
-for programa in filmes_e_series: #Polimorfismo
+for programa in playlist_fim_de_semana.programas: #Polimorfismo
     print(programa) #Calls __str__
