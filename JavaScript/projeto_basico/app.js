bbfba4a28569fe2d1e3ffbe7019905c5e1290c6b
Griffo <marceloramalho.info@gmail.com>
2024-12-17 20:59:10 -0300

    creating a prompt for the first time

--- JavaScript/projeto_basico/app.js
+++ JavaScript/projeto_basico/app.js
@@ -1,1 +1,7 @@
-alert('Hello World!'); 
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 29;//In JavaScript we use camelCase
+prompt('Escolha um número entre 1 e 30')
+
+//camelCase
+//snake_case
+//PascalCase
