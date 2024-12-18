b3f57d7b338fe0edfd9f1741e683d7c223ac6d19
Griffo <marceloramalho.info@gmail.com>
2024-12-17 21:10:57 -0300

    Implementing an else if user gives a wrong anwser

--- JavaScript/projeto_basico/app.js
+++ JavaScript/projeto_basico/app.js
@@ -1,9 +1,12 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 29;//In JavaScript we use camelCase
+console.log('O numero secreto é ' + numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 30')
 
 if (chute == numeroSecreto) {
-    console.log('Parabens, vocé acertou'); //Mensagem exibida no console
+    alert('Parabens, vocé acertou'); //Mensagem exibida no console
+} else {
+    alert('Que pena, vocé errou :(')
 }
 //camelCase
 //snake_case
