7efab19e02341ebcbb2995113f77c6e2e208b4dd
Griffo <marceloramalho.info@gmail.com>
2024-12-17 21:25:50 -0300

    Creating more conditionals if the user gets the wrong awnser

--- JavaScript/projeto_basico/app.js
+++ JavaScript/projeto_basico/app.js
@@ -1,12 +1,16 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 29;//In JavaScript we use camelCase
+let numeroSecreto = 13;//In JavaScript we use camelCase
 console.log('O numero secreto é ' + numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 30')
 
 if (chute == numeroSecreto) {
-    alert(`Parabens, vocé acertou o número secreto ${chute}`); //Mensagem exibida no console; Template string
+    alert(`Parabens, vocé acertou o número secreto ${chute}`); //Mensagem exibida no console; Template string pra concatenação
 } else {
-    alert('Que pena, vocé errou :(')
+    if (chute > numeroSecreto) {
+        alert(`O número secreto é menor que ${chute}`)
+    } else {
+        alert(`O número secreto é maior que ${chute}`)
+    }
 }
 //camelCase
 //snake_case
