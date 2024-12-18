73e5d6ac718ae74d83651934e850e99c31200f8a
Griffo <marceloramalho.info@gmail.com>
2024-12-17 21:34:17 -0300

    Working with while

--- JavaScript/projeto_basico/app.js
+++ JavaScript/projeto_basico/app.js
@@ -1,17 +1,24 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 13;//In JavaScript we use camelCase
 console.log('O numero secreto é ' + numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 30')
+let chute
 
-if (chute == numeroSecreto) {
-    alert(`Parabens, vocé acertou o número secreto ${chute}`); //Mensagem exibida no console; Template string pra concatenação
-} else {
-    if (chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}`)
+//enquanto o chute for diferente do numero secreto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 30');
+    if (chute == numeroSecreto) {
+        alert(`Parabens, vocé acertou o número secreto ${chute}`); //Mensagem exibida no console; Template string pra concatenação
     } else {
-        alert(`O número secreto é maior que ${chute}`)
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`)
+        } else {
+            alert(`O número secreto é maior que ${chute}`)
+        }
     }
+        
 }
 //camelCase
 //snake_case
 //PascalCase
+
+//Operadores de comparação > < >= <= == !=
