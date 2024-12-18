ce2cdd71a663359652f37f83e46b06258bab4c1e
Griffo <marceloramalho.info@gmail.com>
2024-12-17 21:04:01 -0300

    creating a conditional to verify is the user awnser the right number

--- JavaScript/projeto_basico/app.js
+++ JavaScript/projeto_basico/app.js
@@ -1,7 +1,10 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 29;//In JavaScript we use camelCase
-prompt('Escolha um número entre 1 e 30')
+let chute = prompt('Escolha um número entre 1 e 30')
 
+if (chute == numeroSecreto) {
+    console.log('Parabens, vocé acertou'); //Mensagem exibida no console
+}
 //camelCase
 //snake_case
 //PascalCase
