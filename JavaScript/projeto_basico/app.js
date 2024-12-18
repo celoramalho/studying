1eb1d5ea1f28f5e4ad8aa5d8c75d204400f49087
Griffo <marceloramalho.info@gmail.com>
2024-12-17 21:38:10 -0300

    criando variavel tentativas e incrementando a cada repeticao/tentativa

--- JavaScript/projeto_basico/app.js
+++ JavaScript/projeto_basico/app.js
@@ -1,21 +1,22 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 13;//In JavaScript we use camelCase
-console.log('O numero secreto é ' + numeroSecreto)
+console.log('O numero secreto é ' + numeroSecreto); // Linter puts automatically ;
 let chute
+let tentativas = 1;
 
 //enquanto o chute for diferente do numero secreto
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 30');
     if (chute == numeroSecreto) {
-        alert(`Parabens, vocé acertou o número secreto ${chute}`); //Mensagem exibida no console; Template string pra concatenação
+        alert(`Parabens, vocé acertou o número secreto ${chute} com ${tentativas} tentativas`); //Mensagem exibida no console; Template string pra concatenação
     } else {
         if (chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`)
+            alert(`O número secreto é menor que ${chute}`);
         } else {
-            alert(`O número secreto é maior que ${chute}`)
+            alert(`O número secreto é maior que ${chute}`);
         }
+        tentativas++; //tentativas = tentativas + 1; ++ para incrementar um
     }
-        
 }
 //camelCase
 //snake_case
