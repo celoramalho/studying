1afb6b877cd7ac0eeb385d3a06b54c6f40ff52cd
Griffo <marceloramalho.info@gmail.com>
2024-12-23 17:48:12 -0300

    Adding responsiveVoice to the game, a voice that reads the messages prompted

--- JavaScript/projeto-basico-2/js-curso-2-aula_-/app.js
+++ JavaScript/projeto-basico-2/js-curso-2-aula_-/app.js
@@ -11,6 +11,8 @@ let numeroSecreto = gerarNumeroAleatorio();
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female',
+    {rate:1.2});
 }
 
 function exibirMensagemInicial(){
