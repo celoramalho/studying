86dec739cde6d7ea44670b9091d9a20a61877393
Griffo <marceloramalho.info@gmail.com>
2024-12-23 18:46:12 -0300

    Sorting numbers app.js

---
+++ JavaScript/sorteador-numeros/app.js
@@ -0,0 +1,33 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value
+    let doNumero = document.getElementById('de').value
+    let ateNumero = document.getElementById('ate').value
+
+    console.log(`Quantidade: ${quantidade}`);
+    console.log(`Do número: ${doNumero}`);
+    console.log(`Até o número: ${ateNumero}`);
+
+    let numerosSorteados = [];
+
+    while (numerosSorteados.length < quantidade) {
+        let numeroSorteado = parseInt(Math.random() * (ateNumero) + doNumero);
+        if (numerosSorteados.includes(numeroSorteado)) {
+            continue
+        }else{
+            console.log(`numeroSorteado: ${numeroSorteado}`);
+            numerosSorteados.push(numeroSorteado);
+        }
+    }
+    exibirNaTela('#resultado', numerosSorteados);
+    document.getElementById('btn-reiniciar').removeAttribute('disabled');
+}
+function exibirNaTela(id, texto) {
+    let campo = document.querySelector(id);
+    campo.innerHTML = texto;
+}
+
+function reiniciar() {
+    exibirNaTela('#resultado', 'Números sorteados:  nenhum até agora');
+    document.getElementById('btn-reiniciar').setAttribute('disabled', true);
+    document.getElementById('btn-sortear').removeAttribute('disabled');
+}
