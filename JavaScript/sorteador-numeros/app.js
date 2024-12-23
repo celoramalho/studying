f1a9da7552701061c145b898c6f219e6f746e6c6
Griffo <marceloramalho.info@gmail.com>
2024-12-23 19:02:26 -0300

    Refatorando e criando validacao de inputs para verificar se é possivel sortear os numeros com os parametros recebidos

--- JavaScript/sorteador-numeros/app.js
+++ JavaScript/sorteador-numeros/app.js
@@ -3,16 +3,21 @@ function sortear() {
     let doNumero = parseInt(document.getElementById('de').value)
     let ateNumero = parseInt(document.getElementById('ate').value)
 
+    if (validaInputs(quantidade, doNumero, ateNumero)){
+        pass
+    }else{
+        return
+    }
+
     console.log(`Quantidade: ${quantidade}`);
     console.log(`Do número: ${doNumero}`);
     console.log(`Até o número: ${ateNumero}`);
 
     let numerosSorteados = [];
-
     while (numerosSorteados.length < quantidade) {
-        let numeroSorteado = parseInt(Math.random() * (ateNumero) + doNumero);
+        numeroSorteado = obterNumerosSorteados(doNumero, ateNumero);
         if (numerosSorteados.includes(numeroSorteado)) {
-            continue
+            continue;
         }else{
             console.log(`numeroSorteado: ${numeroSorteado}`);
             numerosSorteados.push(numeroSorteado);
@@ -21,6 +26,24 @@ function sortear() {
     exibirNaTela('#resultado', numerosSorteados);
     document.getElementById('btn-reiniciar').removeAttribute('disabled');
 }
+
+function validaInputs(quantidade, doNumero, ateNumero) {
+    if  (doNumero > ateNumero)  {
+        alert('Número mínimo do sorteio deve ser menor que o número máximo');
+        return false;
+    }else if (quantidade > ateNumero - doNumero) {
+        alert('Quantidade de números sorteados deve ser menor que o número máximo - número mínimo');
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function obterNumerosSorteados(min, max) {
+    let numeroSorteado = parseInt(Math.random() * (max - min) + min);   
+    return numeroSorteado;
+}
+
 function exibirNaTela(id, texto) {
     let campo = document.querySelector(id);
     campo.innerHTML = texto;
