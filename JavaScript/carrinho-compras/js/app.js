56229eccd2bd84389e390415dd9f168d22ee7658
Griffo <marceloramalho.info@gmail.com>
2024-12-24 10:18:04 -0300

    Fazendo o js do projeto carrinho de compra

---
+++ JavaScript/carrinho-compras/js/app.js
@@ -0,0 +1,51 @@
+
+let inputQuantidade = document.getElementById('quantidade');
+inputQuantidade.value = 1;
+function adicionar() {
+    let produto = document.getElementById('produto').value;
+    let quantidade = document.getElementById('quantidade').value;
+
+    console.log(`Produto: ${produto}`);
+    console.log(`Quantidade: ${quantidade}`);
+
+    adicionarProdutoAoCarrinho(produto, quantidade);
+}
+
+function adicionarProdutoAoCarrinho(produto, quantidade) {
+    let listaProduto = document.getElementById('lista-produtos');
+    let valor = produto.split('-')[1];
+    let nomeProduto = produto.split('-')[0];
+    
+    if (quantidade <= 0){
+        alert('Quantidade deve ser maior que zero para adicionar ao carrinho');
+        return;
+    }else{
+        listaProduto.innerHTML += `
+        <section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul"> ${valor}</span>
+        </section>
+        `;
+        somarValorTotal(valor, quantidade);
+    }
+
+    
+}
+
+function limpar() {
+    let listaProduto = document.getElementById('lista-produtos');
+    listaProduto.innerHTML = '';
+
+    let valorTotal = document.getElementById('valor-total');
+    valorTotal.textContent = 'R$0';
+}
+
+function somarValorTotal(valor, quantidade) {
+    let valorTotal = document.getElementById('valor-total')
+    let valorAtual = valorTotal.textContent.split('$')[1];
+    let valorProduto = valor.split('$')[1];
+
+    let novoValor = parseInt(valorAtual) + parseInt(valorProduto)*parseInt(quantidade);
+    console.log(valorAtual);
+    console.log(novoValor);
+    valorTotal.textContent = `R$${novoValor}`;
+}
