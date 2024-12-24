39b470a4698a3f5c2758e6ab8825909b010c29d5
Griffo <marceloramalho.info@gmail.com>
2024-12-23 23:02:17 -0300

    Alterando nome da varivael link_alugar pra linkAlugar

--- JavaScript/alugames/js/app.js
+++ JavaScript/alugames/js/app.js
@@ -1,16 +1,16 @@
 function alterarStatus(id) {
     let game = document.getElementById(`game-${id}`);
     let img = game.querySelector('.dashboard__item__img');
-    let link_alugar = game.querySelector('.dashboard__item__button');
+    let linkAlugar = game.querySelector('.dashboard__item__button');
 
-    if (link_alugar.classList.contains('dashboard__item__button--return')){
+    if (linkAlugar.classList.contains('dashboard__item__button--return')){
         img.classList.remove('dashboard__item__img--rented');
-        link_alugar.classList.remove('dashboard__item__button--return');
-        link_alugar.textContent = 'Alugar';
+        linkAlugar.classList.remove('dashboard__item__button--return');
+        linkAlugar.textContent = 'Alugar';
     }
     else{
         img.classList.add('dashboard__item__img--rented');
-        link_alugar.classList.add('dashboard__item__button--return');
-        link_alugar.textContent = 'Devolver';
+        linkAlugar.classList.add('dashboard__item__button--return');
+        linkAlugar.textContent = 'Devolver';
     }
 }
