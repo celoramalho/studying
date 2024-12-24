566d08cbbe4d02b54077ec2ea591df1e48a0642d
Griffo <marceloramalho.info@gmail.com>
2024-12-23 23:08:42 -0300

    Alterando nome da varivael linkAlugar pra botao

--- JavaScript/alugames/js/app.js
+++ JavaScript/alugames/js/app.js
@@ -1,18 +1,18 @@
 function alterarStatus(id) {
     let game = document.getElementById(`game-${id}`);
     let img = game.querySelector('.dashboard__item__img');
-    let linkAlugar = game.querySelector('.dashboard__item__button');
+    let botao = game.querySelector('.dashboard__item__button');
     let nomeJogo =  game.querySelector('.dashboard__item__name');
     console.log(`Jogo: ${nomeJogo.textContent}`);
-    
-    if (linkAlugar.classList.contains('dashboard__item__button--return')){
+
+    if (botao.classList.contains('dashboard__item__button--return')){
         img.classList.remove('dashboard__item__img--rented');
-        linkAlugar.classList.remove('dashboard__item__button--return');
-        linkAlugar.textContent = 'Alugar';
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
     }
     else{
         img.classList.add('dashboard__item__img--rented');
-        linkAlugar.classList.add('dashboard__item__button--return');
-        linkAlugar.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
     }
 }
