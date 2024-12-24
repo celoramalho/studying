8d846afc5cf1bd73e468f0934cd5ec5daef1a23c
Griffo <marceloramalho.info@gmail.com>
2024-12-23 23:03:57 -0300

    Criando um log no console pra mostrar o nome do jogo que foi clicado

--- JavaScript/alugames/js/app.js
+++ JavaScript/alugames/js/app.js
@@ -2,7 +2,9 @@ function alterarStatus(id) {
     let game = document.getElementById(`game-${id}`);
     let img = game.querySelector('.dashboard__item__img');
     let linkAlugar = game.querySelector('.dashboard__item__button');
-
+    let nomeJogo =  game.querySelector('.dashboard__item__name');
+    console.log(`Jogo: ${nomeJogo.textContent}`);
+    
     if (linkAlugar.classList.contains('dashboard__item__button--return')){
         img.classList.remove('dashboard__item__img--rented');
         linkAlugar.classList.remove('dashboard__item__button--return');
