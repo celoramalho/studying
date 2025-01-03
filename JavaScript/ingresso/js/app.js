337ae759f0013e9f49426d4240f29587085c7433
anubis_greyignis <marceloramalho.info@gmail.com>
2025-01-03 16:00:32 -0300

    Verificando se a quantidade informada de ingressos é maior que 0, contudo, válida

--- JavaScript/ingresso/js/app.js
+++ JavaScript/ingresso/js/app.js
@@ -1,6 +1,12 @@
+resetar();
+
 function comprar(){
     let tipoIngresso = document.querySelector('#tipo-ingresso').value;
     let quantidadeIngresso = document.querySelector('#qtd').value;
+
+    if (quantidadeIngresso <= 0) {
+        alert('Quantidade deve ser maior que 0');
+    }
 
     console.log(`Tipo de ingresso: ${tipoIngresso}`);
     console.log(`Quantidade: ${quantidadeIngresso}`);
@@ -17,15 +23,32 @@ function alterarQuantidadeEstoque(tipoIngresso, quantidadeIngresso){
         return
     }else{
         if(novaQuantidade == 0){
-            alterarBotaoCompra();
+            alterarBotaoCompra('disable');
         }
         alert(`Compra realizada com sucesso! ${quantidadeIngresso} ingressos ${tipoIngresso} comprados`);
         quantidadeEstoque.textContent = novaQuantidade;
-    }    
+    }
+    alterarBotaoCompra('enable');    
 }
 
-function alterarBotaoCompra(){
+function alterarBotaoCompra(status){
     let botao = document.querySelector('#botao-comprar');
-    botao.setAttribute('disabled', true);
-    botao.classList.add('botao-desabilitado');
+
+    if (status == 'enable'){
+        botao.classList.remove('botao-desabilitado');
+        botao.removeAttribute('disabled');
+    }else{
+        botao.setAttribute('disabled', true);
+        botao.classList.add('botao-desabilitado');
+    }
+}
+
+function resetar(){
+    let ingressoPista = document.querySelector('#qtd-pista').value;
+    let ingressoSuperior = document.querySelector('#qtd-superior').value;
+    let ingressoInferior = document.querySelector('#qtd-inferior').value;
+    ingressoPista.textContent = 100;
+    ingressoSuperior.textContent = 200;
+    ingressoInferior.textContent = 400;
+    alterarBotaoCompra('enable');
 }
