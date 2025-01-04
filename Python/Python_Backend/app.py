cd389bc76026b230d2e991d6022a65bfa40ebcd2
Griffo <marceloramalho.info@gmail.com>
2025-01-03 22:59:31 -0300

    Printando opcao escolhida

--- Python/Python_Backend/app.py
+++ Python/Python_Backend/app.py
@@ -1,8 +1,10 @@
 print('Sabor Express\n')
 
 print('1. Cadastrar restaurante')
-print('2 Listar restaurantes')
+print('2. Listar restaurantes')
 print('3. Ativar restaurantes')
 print('4. Sair\n')
 
-escolha = input('Escolha uma opção: ')
+opcao_escolhida = input('Escolha uma opção: ')
+print('Você escolheu: ', opcao_escolhida)
+
