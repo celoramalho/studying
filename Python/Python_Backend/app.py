ed86680d0043661321de02ee4530d76e3429ba14
Griffo <marceloramalho.info@gmail.com>
2025-01-03 23:23:15 -0300

    Usando condicionais em python

--- Python/Python_Backend/app.py
+++ Python/Python_Backend/app.py
@@ -16,7 +16,16 @@ print('4. Sair\n')
 opcao_escolhida = input('Escolha uma opção: ')
 print(f'Você escolheu: {opcao_escolhida}') #interpolação de string
 
-
+if opcao_escolhida == '1':
+    print('Cadastrar restaurante')
+elif opcao_escolhida == '2':
+    print('Listar restaurantes')
+elif opcao_escolhida == '3':
+    print('Ativar restaurantes')
+elif opcao_escolhida == '4':
+    print('Sair')
+else:
+    print('Opção inválida')
 '''
 aspas simples tripla
 '''
@@ -63,5 +72,53 @@ If the implementation is easy to explain, it may be a good idea.\
 Namespaces are one honking great idea -- let's do more of those!\
     
 """
+
+
+
+"""
+The Zen of Python, by Tim Peters em português
+
+Bonito é melhor que feio.\
+
+Explícito é melhor que implícito.\
+
+Simples é melhor que complexo.\
+
+Complexo é melhor que complicado.\
+
+Plano é melhor que aninhado.\
+
+Esparso é melhor que denso.\
+
+Legibilidade conta.\
+
+Casos especiais não são especiais o bastante para quebrar as regras.\
+
+Embora praticidade vença a pureza.\
+
+Erros nunca devem passar silenciosamente.\
+
+A menos que sejam explicitamente silenciados.\
+
+Diante da ambiguidade, recuse a tentação de adivinhar.\
+
+Deveria haver uma — e preferencialmente só uma — maneira óbvia de fazer algo.\
+
+Embora essa maneira possa não ser óbvia a princípio a menos que você seja holandês.\
+
+Agora é melhor que nunca.\
+
+Embora nunca seja frequentemente melhor que agora mesmo.\
+
+Se a implementação é difícil de explicar, é uma má ideia.\
+
+Se a implementação é fácil de explicar, pode ser uma boa ideia.\
+
+Namespaces são uma grande ideia — vamos fazer mais dessas!\
+        
+"""
+
+#sep = '\n' separador da função print
+
 
 # __dunder__ methods
