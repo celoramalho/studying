b6ae5acdc97b0caa4d2f5ff7c79fd476cb4f7203
Griffo <marceloramalho.info@gmail.com>
2025-01-04 12:33:07 -0300

    Começando a criar classe restaurante, aprendendo como funciona os objetos

---
+++ Python/Python_Backend/SaborExpress_OO/modelos/restaurante.py
@@ -0,0 +1,16 @@
+class Restaurante:
+    nome = '' 
+    categoria = '' 
+    ativo = False
+restaurante_praca = Restaurante()
+restaurante_pizza = Restaurante()
+
+restaurantes = [restaurante_praca, restaurante_pizza]
+
+print(restaurantes)
+
+
+
+#Objeto é uma instância de uma classe, que armazena dados e permite a execução de métodos.
+
+#[<__main__.Restaurante object at 0x7f0f67cd0760>, <__main__.Restaurante object at 0x7f0f67cd0730>] endereço na memoria
