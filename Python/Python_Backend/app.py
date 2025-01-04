c4ddb4217d7921a17e4c3e076e6aaa75b84db4a6
Griffo <marceloramalho.info@gmail.com>
2025-01-03 23:02:18 -0300

    Usando aspas triplas

--- Python/Python_Backend/app.py
+++ Python/Python_Backend/app.py
@@ -1,4 +1,6 @@
-print('Sabor Express\n')
+print("""
+      Sabor Express
+      """)
 
 print('1. Cadastrar restaurante')
 print('2. Listar restaurantes')
@@ -6,5 +8,5 @@ print('3. Ativar restaurantes')
 print('4. Sair\n')
 
 opcao_escolhida = input('Escolha uma opção: ')
-print('Você escolheu: ', opcao_escolhida)
+print("Você escolheu: ", opcao_escolhida)
 
