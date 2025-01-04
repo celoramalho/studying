30c73d59145935df1225259d340e966edf505f1b
Griffo <marceloramalho.info@gmail.com>
2025-01-03 21:53:38 -0300

    Renomeando pasta Python_Joguinhos e adicionando input no arquivo app.py na pasta Python_backend

--- Python/Python_Backend/app.py
+++ Python/Python_Backend/app.py
@@ -3,4 +3,6 @@ print('Sabor Express\n')
 print('1. Cadastrar restaurante')
 print('2 Listar restaurantes')
 print('3. Ativar restaurantes')
-print('4. Sair')
+print('4. Sair\n')
+
+escolha = input('Escolha uma opção: ')
