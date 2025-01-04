eeaa84f585dbd1e7d3062657d76c37619fac0ede
Griffo <marceloramalho.info@gmail.com>
2025-01-03 23:09:57 -0300

    The Zen of Python aspas triplas

--- Python/Python_Backend/app.py
+++ Python/Python_Backend/app.py
@@ -14,11 +14,54 @@ print('3. Ativar restaurantes')
 print('4. Sair\n')
 
 opcao_escolhida = input('Escolha uma opção: ')
-print(f"Você escolheu: {opcao_escolhida}") #interpolação de string
+print(f'Você escolheu: {opcao_escolhida}') #interpolação de string
 
 
 '''
 aspas simples tripla
 '''
+"""
+aspas duplas tripla
+
+The Zen of Python, by Tim Peters
+Beautiful is better than ugly.\
+
+Explicit is better than implicit.\
+
+Simple is better than complex.\
+
+Complex is better than complicated.\
+
+Flat is better than nested.\
+
+Sparse is better than dense.\
+
+Readability counts.\
+
+Special cases aren't special enough to break the rules.\
+
+Although practicality beats purity.\
+
+Errors should never pass silently.\
+
+Unless explicitly silenced.\
+
+In the face of ambiguity, refuse the temptation to guess.\
+
+There should be one-- and preferably only one --obvious way to do it.\
+
+Although that way may not be obvious at first unless you're Dutch.\
+
+Now is better than never.\
+
+Although never is often better than right now.\
+
+If the implementation is hard to explain, it's a bad idea.\
+
+If the implementation is easy to explain, it may be a good idea.\
+
+Namespaces are one honking great idea -- let's do more of those!\
+    
+"""
 
 # __dunder__ methods
