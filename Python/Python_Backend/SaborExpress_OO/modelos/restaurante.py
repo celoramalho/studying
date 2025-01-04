4af9d194dab60fe14bb4b57022bc902cbf077149
Griffo <marceloramalho.info@gmail.com>
2025-01-04 18:18:24 -0300

    Passando valores para os atributos da classe restaurante, usando vars e dir pra ver os args, properties e methods

--- Python/Python_Backend/SaborExpress_OO/modelos/restaurante.py
+++ Python/Python_Backend/SaborExpress_OO/modelos/restaurante.py
@@ -2,13 +2,32 @@ class Restaurante:
     nome = '' 
     categoria = '' 
     ativo = False
-restaurante_praca = Restaurante()
+    
+restaurante_subuai = Restaurante()
+restaurante_subuai.nome = 'Subuai'
+restaurante_subuai.categoria = 'Fast Food'
+restaurante_subuai.ativo = True
+
 restaurante_pizza = Restaurante()
 
-restaurantes = [restaurante_praca, restaurante_pizza]
+restaurantes = [restaurante_subuai, restaurante_pizza]
 
-print(restaurantes)
+print(restaurante_subuai.ativo)
 
+
+
+
+
+
+# print(vars(restaurante_subuai))
+# {'nome': 'Subuai', 'categoria': 'Fast Food', 'ativo': True}
+
+"""
+print(dir(restaurante_subuai))
+['__add__', '__class__', '__class_getitem__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
+
+
+"""
 
 
 #Objeto é uma instância de uma classe, que armazena dados e permite a execução de métodos.
