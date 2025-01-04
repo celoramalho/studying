2320c3241484945200a12df169c82fdd6e9d92b0
Griffo <marceloramalho.info@gmail.com>
2025-01-04 18:31:49 -0300

    Criando atributo da classe, lista de restaurantes

--- Python/Python_Backend/SaborExpress_OO/modelos/restaurante.py
+++ Python/Python_Backend/SaborExpress_OO/modelos/restaurante.py
@@ -1,26 +1,25 @@
 class Restaurante:
-    nome = '' 
-    categoria = '' 
-    ativo = False
+    restaurantes = []
     def __init__(self, nome, categoria): # Construtor
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self)
         
     
     
     def __str__(self):
         return f'{self.nome} | {self.categoria} | {self.ativo}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(restaurante)
         
     
 restaurante_subuai = Restaurante('Subuai', 'Fast Food')
 restaurante_osteria = Restaurante('74 Osteria', 'Osteria')
 
-restaurante_subuai.ativo = True
-
-restaurantes = [restaurante_subuai, restaurante_osteria]
-
-print(restaurante_subuai)
+print(Restaurante.listar_restaurantes())
 
 
 
