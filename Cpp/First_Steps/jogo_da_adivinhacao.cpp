9a6cfc96fe0548424d81f6568e74031092282bf2
Marcelo Ramalho <marceloramalho.info@gmail.com>
2025-01-05 19:57:19 -0300

    Usando condicionais para verificar o chute e comentarios em cpp

--- Cpp/First_Steps/jogo_da_adivinhacao.cpp
+++ Cpp/First_Steps/jogo_da_adivinhacao.cpp
@@ -6,4 +6,23 @@ int main(){
     cout << "*************************************" << endl;
     cout << "* Bem-Vindos ao jogo da adivinhação *" << endl;
     cout << "* ***********************************" << endl;
+
+    int numero_secreto = 42;
+    // 42cout << "O número secreto é " << numero_secreto << endl;
+
+    int chute;
+    cout << "Qual seu chute: ";
+    cin >> chute;
+    cout << "O seu chute foi " << chute << endl;
+
+    if (chute == numero_secreto){
+        cout << "Parabéns! Vocé acertou!" << endl;
+    }
+    else if(chute > numero_secreto){
+        cout << "O seu chute foi maior que o número secreto" << endl;
+    }
+    else{
+        cout << "O seu chute foi menor que o número secreto" << endl;
+    }
+
 }
