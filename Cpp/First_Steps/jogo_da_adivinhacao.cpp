a564b2a59fc75ffeb0dfdfe859618325cf60aa81
Marcelo Ramalho <marceloramalho.info@gmail.com>
2025-01-05 20:09:14 -0300

    Usando estrutura de repeticao while para repetir o jogo até o usuario acertar o número. Variavel tantativas é incrementada a cada repetição

--- Cpp/First_Steps/jogo_da_adivinhacao.cpp
+++ Cpp/First_Steps/jogo_da_adivinhacao.cpp
@@ -10,22 +10,30 @@ int main(){
     const int NUMERO_SECRETO = 42; // IMUTAVEL
     // cout << "O número secreto é " << numero_secreto << endl;
 
-    int chute;
-    cout << "Qual seu chute: ";
-    cin >> chute;
-    cout << "O seu chute foi " << chute << endl;
+    bool nao_acertou = true;
+    int tentativas = 0;
 
+    while(nao_acertou){
+        tentativas++;
+        int chute;
+        cout << "####Tentativa " << tentativas << "####" << endl;
+        cout << "Qual seu chute: ";
+        cin >> chute;
 
-    bool acertou = (chute == NUMERO_SECRETO);
-    bool maior = chute > NUMERO_SECRETO;
-    
-    if (acertou){
-        cout << "Parabéns! Vocé acertou!" << endl;
+        bool acertou = (chute == NUMERO_SECRETO);
+        bool maior = chute > NUMERO_SECRETO;
+        
+        if (acertou){
+            cout << "Parabéns! Vocé acertou!" << endl;
+            nao_acertou = false;
+        }
+        else if(maior){
+            cout << "O seu chute foi maior que o número secreto" << endl;
+        }
+        else{
+            cout << "O seu chute foi menor que o número secreto" << endl;
+        }
     }
-    else if(maior){
-        cout << "O seu chute foi maior que o número secreto" << endl;
-    }
-    else{
-        cout << "O seu chute foi menor que o número secreto" << endl;
-    }
+    cout << "Fim de jogo" << endl;
+    cout << "Você acertou o número secreto em " << tentativas << " tentativas" << endl;
 }
