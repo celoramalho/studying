ed65dd2db28678d3cbe3fc7fa51a60708cb81d69
Griffo <marceloramalho.info@gmail.com>
2025-01-13 22:56:57 -0300

    Creating route to add games into the list with Flask

--- Python/Flask/jogoteca/jogoteca.py
+++ Python/Flask/jogoteca/jogoteca.py
@@ -7,19 +7,32 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
+
 app = Flask(__name__)
 
-@app.route('/inicio')
+
+@app.route("/inicio")
 def ola():
-    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
-    jogo3 = Jogo('Journey', 'Aventura', 'PS3')
-    
+    jogo1 = Jogo("Tetris", "Puzzle", "Atari")
+    jogo2 = Jogo("God of War", "Rack n Slash", "PS2")
+    jogo3 = Jogo("Journey", "Aventura", "PS3")
+
     lista_jogos = [jogo1, jogo2, jogo3]
-    return render_template('lista.html', titulo='Jogos', jogos=lista_jogos)
+    return render_template("lista.html", titulo="Jogos", jogos=lista_jogos)
+
+@app.route("/novo")
+def novo():
+    return render_template("novo.html", titulo="Novo Jogo")
+
 
 app.run()
 
 
-
 # In dev -> app.run(host='0.0.0.0', port=8080) Don't use this definitions in prod
+
+"""
+Tipos de Delimitadores do Jinja2:
+{%....%}: usado para inserir estruturas Python dentro de um arquivo html;
+{{....}}: usado para facilitar a exibição de código python como um output em um template HTML. Alternativa: {% print(....) %};
+{#....#}: usado para adicionar comentários que não serão exibidos no output do template HTML.
+"""
