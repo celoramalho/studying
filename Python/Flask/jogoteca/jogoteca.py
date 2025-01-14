ac1968f5e72c858ce3d57c2152dabca07fb1ec11
Griffo <marceloramalho.info@gmail.com>
2025-01-13 23:07:04 -0300

    Using POST route to add a game into the game list, renaming index route to just slash, turning into the default route when access the app domain

--- Python/Flask/jogoteca/jogoteca.py
+++ Python/Flask/jogoteca/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 class Jogo:
@@ -8,24 +8,36 @@ class Jogo:
         self.console = console
 
 
+jogo1 = Jogo("Tetris", "Puzzle", "Atari")
+jogo2 = Jogo("God of War", "Rack n Slash", "PS2")
+jogo3 = Jogo("Journey", "Aventura", "PS3")
+
+lista_jogos = [jogo1, jogo2, jogo3]
+
 app = Flask(__name__)
 
 
-@app.route("/inicio")
-def ola():
-    jogo1 = Jogo("Tetris", "Puzzle", "Atari")
-    jogo2 = Jogo("God of War", "Rack n Slash", "PS2")
-    jogo3 = Jogo("Journey", "Aventura", "PS3")
-
-    lista_jogos = [jogo1, jogo2, jogo3]
+@app.route("/")
+def index():
     return render_template("lista.html", titulo="Jogos", jogos=lista_jogos)
 
 @app.route("/novo")
 def novo():
     return render_template("novo.html", titulo="Novo Jogo")
 
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    
+    jogo = Jogo(nome, categoria, console)
+    lista_jogos.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista_jogos)
+    
 
-app.run()
+
+app.run(debug=True) #debug=True auto reload
 
 
 # In dev -> app.run(host='0.0.0.0', port=8080) Don't use this definitions in prod
