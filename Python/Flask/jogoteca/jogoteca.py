afdbeaa5cd242eeddf0595bf6ffe04f3d2632bed
anubis_greyignis <marceloramalho.info@gmail.com>
2025-01-13 22:13:39 -0300

    Testando passar variaveis para o HTML com flask

--- Python/Flask/jogoteca/jogoteca.py
+++ Python/Flask/jogoteca/jogoteca.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template
 
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
+    lista_jogos = ['Tetris', 'Skyrim', 'Crash']
+    return render_template('lista.html', titulo='Jogos', jogos=lista_jogos)
 
 app.run()
+
+
+
+# In dev -> app.run(host='0.0.0.0', port=8080) Don't use this definitions in prod
